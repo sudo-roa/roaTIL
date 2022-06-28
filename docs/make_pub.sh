5ed9f4c239de0c4c7b6724003fb962f91a29c08c
sudo-roa <sudo.roa9393@gmail.com>
2022-06-28 21:42:08 +0900

    update roaTIL

---
+++ docs/make_pub.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# make LearnedTree.md file
+echo "" > source/LearnedTree.md
+echo "# Learned tree" >> source/LearnedTree.md
+echo '```' >> source/LearnedTree.md
+tree source -I "*.nouse|_*|mytheme|index.rst|image" >> source/LearnedTree.md
+echo '```' >> source/LearnedTree.md
+
+# publish
+make html
